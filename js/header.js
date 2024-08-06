2185071569a5d5b779488d4571b3349b25959410
accguy <accguy2190@naver.com>
2024-08-06 10:48:48 +0900

    로그인 버튼의 클래스명 변경하기 수정

--- js/header.js
+++ js/header.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const authToken = localStorage.getItem("authToken");
   const $loginButton = document.querySelector(".login-btn");
+  const $mypageButton = document.querySelector(".mypage-btn");
 
   if (authToken) {
     $loginButton.querySelector("span").innerText = "마이페이지";
     $loginButton.href = "index.html"; // 마이페이지 링크로 변경
-    $loginButton.classList.remove("login-btn");
     $loginButton.classList.add("mypage-btn");
+    $loginButton.classList.remove("login-btn");
   } else {
-    $loginButton.querySelector("span").innerText = "로그인";
-    $loginButton.href = "login.html"; // 로그인 페이지 링크로 변경
-    $loginButton.classList.remove("mypage-btn");
-    $loginButton.classList.add("login-btn");
+    $mypageButton.querySelector("span").innerText = "로그인";
+    $mypageButton.href = "login.html"; // 로그인 페이지 링크로 변경
+    $mypageButton.classList.add("login-btn");
+    $mypageButton.classList.remove("mypage-btn");
   }
 });
